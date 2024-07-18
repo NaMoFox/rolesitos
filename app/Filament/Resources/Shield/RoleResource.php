1fd30ab1f2caf68ccca8130575ff264c8090a5b9
NaMoFox <73260593+NaMoFox@users.noreply.github.com>
2024-07-18 07:25:15 -0500

    Update RoleResource.php

--- app/Filament/Resources/Shield/RoleResource.php
+++ app/Filament/Resources/Shield/RoleResource.php
@@ -21,7 +21,7 @@ use Illuminate\Support\Str;
 class RoleResource extends Resource implements HasShieldPermissions
 {
     protected static ?string $recordTitleAttribute = 'name';
-
+   protected static ?string $tenantOwnershipRelationshipName = 'team';
     
     public static function getPermissionPrefixes(): array
     {
@@ -34,7 +34,7 @@ class RoleResource extends Resource implements HasShieldPermissions
             'delete_any',
         ];
     }
-   protected static ?string $tenantOwnershipRelationshipName = 'team';
+
     public static function form(Form $form): Form
     {
         return $form
